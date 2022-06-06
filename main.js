6b64f121ea888aec99bc915e4871b5317efcfc47
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-06-06 00:35:11 -0300

    Reestructurando logica de la app a partir  de pruebas en grasshopper

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-/* alert('probando js');
- */
-
-/* console.log('console test');
- */
 
 
 function ingresoGasto(){
-    console.log('Etapa 1, incluimos los gastos y los leemos en consola.');
 
-    let name = [];
-    let paid = [];
-
-    name =+ document.getElementById('name').value;
-    paid =+ document.getElementById('paid').value;
-
-    console.log(name,paid)
-/*     for (let element of name) {
-        console.log(element);
-    }
-    for (let element of paid) {
-        console.log(element);
-    }
- */
-    console.log('fin de ejemplo');
 };
 
-/* ingresoGasto() */
