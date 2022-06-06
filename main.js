7ac285138253d6a7ddc6ebadced9c30269b22393
NicolasKorzusehec <nkorzusehec@gmail.com>
2022-06-06 04:46:12 -0300

    Se habia roto try again, era un problema en ingresoGasto y un operador de igualdad

--- main.js
+++ main.js
@@ -10,32 +10,30 @@ var paid=[];
 var fullAmount = 0;
 var averagePerUser = 0;
 
-/* Ejecutamos la salida predeterminada cuando no hay entradas. */
-printExit();
-
+/*Analiza si falta completar algun campo, de ser asi manda una alerta y termina de ejecutar; de lo contrario incluye losparametros en los arreglos y llama la funcion updateExit */
 function ingresoGasto(){
     let person = document.getElementById('users').value;
-    let amount = parseFloat( document.getElementById('paid').value )
+    let amount = parseFloat( document.getElementById('paid').value );
     
-    if (person === undefined || amount === undefined) {
+    if ( person == [] || amount == [] ){
         tryAgain();
     } else {
     users.push(person);
         console.log(users);
     paid.push(amount);
         console.log(paid);
-    updateExit();
+    updateExit(person, amount);
     };  
 };
 
 function tryAgain(){
     alert("It's necesary to complete both fields. Please try Again.");
-    return;
+    console.log ('tryAgain funciona correctamente.');
 };
 
-function updateExit(){
+function updateExit(i, cash){
     average();
-    printExit();
+    printExit(i, cash);
 };
 
 function average(){
@@ -50,26 +48,79 @@ function average(){
     console.log('promedio por persona es ' + averagePerUser);
 };
 
-function printExit(){
+function printExit(i, cash){
     if( users == false && paid == false ){
         console.log("html. ventana base en la que figure no data loaded yet");
+
+        let defaultshow = document.getElementById('default');
+        defaultshow.innerHTML = `
+        <p>No data loaded yet</p> 
+        `;
+
+        let enteredData = document.getElementById('entereddata');
+        enteredData.innerHTML = "";
+        let results = document.getElementById('results');
+        results.innerHTML = "";
+
     } else {
-        clearExit();   
-        exitTable();
-        exitResults();
+        clearExit(i, cash);   
     };
 };
 
-function clearExit(){
-    console.log('html. Salida teoricamente reseteada. se borro todo del div salida')
-}
+function clearExit(i, cash){
+    let defaultshow = document.getElementById('default');
+    defaultshow.innerHTML = "";
 
-function exitTable(){
-    console.log('html. Tabla con los valores que se ingresaron lograda');
+    let enteredData = document.getElementById('entereddata');
+    enteredData.innerHTML = "";
+
+    let results = document.getElementById('results');
+    results.innerHTML = "";
+    console.log('Salida teoricamente reseteada.');
+
+    exitTable(i, cash);
+    exitResults(); 
 };
+
+ function exitTable(i, cash){
+    console.log('html. Tabla con los valores que se ingresaron lograda');
+    let enteredData = document.getElementById('entereddata');
+    enteredData.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${i}: $${cash}</td>
+        </tr>
+    `)
+};
+
+
+/* function exitTable(i, cash){
+    console.log('html. Tabla con los valores que se ingresaron lograda');
+/*     let enteredData = document.getElementById('entereddata');
+    let printTable;
+    
+    for (let index = paid.length; index > -1; index--){
+        printTable += <tr>
+        <td>${users[index]}: $${paid[index]}</td>
+    </tr>
+    }
+    
+    enteredData.innerHTML = `${printTable}`
+ };
+ */
+
 
 function exitResults(){
     console.log('html. Salida con los valores calculados lograda');
+    let results = document.getElementById('results');
+    results.innerHTML = `                            
+    <div class="col-8">
+        <p class="m-0">Total: $${fullAmount}</p>
+        <p class="m-0">Everyone must pay: $${averagePerUser}</p>                      
+    </div>
+    <div class="col-4">
+        <button onclick="reset()">Reset</button>
+    </div>
+    `
 };
 
 function reset(){
